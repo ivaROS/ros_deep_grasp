40431c1a97352f97c8a21f20947126f803cd1a5e
Anina Mu <amu3@gatech.edu>
2018-10-24 13:05:01 -0400

    Working on kinect subscriber

--- scripts/kinect_subscriber.py
+++ scripts/kinect_subscriber.py
@@ -19,14 +19,16 @@ def launch_kinect():
 
 def callback(data):
     print("CALLBACK")
+    print(data._connection_header)
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 def rgb_listener():
     print("KINECT SUBSCRIBER RGB LISTENER")
     rospy.init_node('rgb_listener', anonymous=True)
-    #rospy.Subscriber("/camera/rgb/image_raw", String, callback)
-    rospy.Subscriber("/camera/rgb/camera_info", String, callback)
-    #rospy.spin()
+    rospy.Subscriber("/camera/rgb/image_raw", String, callback, queue_size=10)
+    #rospy.Subscriber("/camera/rgb/camera_info", String, callback)
+    #rospy.Subscriber("/camera/sensor_msgs/CameraInfo", String, callback)
+    rospy.spin()
 
 
 def depth_listener():
@@ -40,6 +42,6 @@ if __name__ == '__main__':
     print("KINECT SUBSCRIBER")
     # launch_kinect()
     rgb_listener()
-    # depth_listener()
+    #depth_listener()
 
 
