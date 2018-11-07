44f660b6f52c7337e208ee78b678506ef9d9382d
Anina Mu <amu3@gatech.edu>
2018-11-06 19:51:10 -0500

    Getting image directly from topic instead of subscriber

--- scripts/kinect_subscriber.py
+++ scripts/kinect_subscriber.py
@@ -2,6 +2,11 @@
 import rospy
 import roslaunch
 from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage, Image
+
+import time
+import matplotlib.pyplot as plt
+from pprint import pprint
 
 
 def launch_kinect():
@@ -11,37 +16,66 @@ def launch_kinect():
 
     launch = roslaunch.scriptapi.ROSLaunch()
     launch.start()
-    
+
     process = launch.launch(node)
-    print process.is_alive()
+    #print process.is_alive()
     process.stop()
 
 
-def callback(data):
-    print("CALLBACK")
-    print(data._connection_header)
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
-def rgb_listener():
-    print("KINECT SUBSCRIBER RGB LISTENER")
-    rospy.init_node('rgb_listener', anonymous=True)
-    rospy.Subscriber("/camera/rgb/image_raw", String, callback, queue_size=10)
-    #rospy.Subscriber("/camera/rgb/camera_info", String, callback)
-    #rospy.Subscriber("/camera/sensor_msgs/CameraInfo", String, callback)
-    rospy.spin()
+#def rgb_callback(data):
+#    print("RGB_CALLBACK")
+#    print(data._connection_header)
+#    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+#    #im = data
+#    #fig, ax = plt.subplots(figsize=(12, 12))
+#    #ax.imshow(im, aspect='equal')
+#    for i in range(0, len(data.data)):
+#        print(data.data[i])
 
 
-def depth_listener():
-    print("KINECT SUBSCRIBER DEPTH LISTENER")
-    rospy.init_node('depth_listener', anonymous=True)
-    rospy.Subscriber("/camera/depth/image_raw", String, callback)
-    rospy.spin()
+#def depth_callback(data):
+#    print("DEPTH CALLBACK")
+
+
+#def rgb_listener():
+#    print("KINECT SUBSCRIBER RGB LISTENER")
+#    #rospy.init_node('rgb_listener', anonymous=True)
+#    sub = rospy.Subscriber("/camera/rgb/image_raw", Image, rgb_callback) #, queue_size=10)
+#    print(sub)
+#    #print(sub.get_num_connections())
+#    #print(sub.name)
+#    #print(sub.data_class)
+#    #print(sub.callback)
+#    pprint(vars(sub))
+#    #print(rospy.core)
+#    #rospy.spin()
+#    #while not rospy.core.is_shutdown():
+#        #rospy.rostime.wallsleep(0.5)
+#    while True:
+#        stop = rospy.core.is_shutdown()
+#        print(stop)
+#        if (stop):
+#            return
+#        time.sleep(0.5)
+
+
+#def depth_listener():
+#    print("KINECT SUBSCRIBER DEPTH LISTENER")
+#    #rospy.init_node('depth_listener', anonymous=True)
+#    rospy.Subscriber("/camera/depth/image_raw", Image, depth_callback)
+#    rospy.spin()
 
 
 if __name__ == '__main__':
-    print("KINECT SUBSCRIBER")
-    # launch_kinect()
-    rgb_listener()
-    #depth_listener()
+    print("MAIN")
+    rospy.init_node("kinect_subscriber")
+#    rgb_listener()
+#    depth_listener()
+    while not rospy.core.is_shutdown():
+        try:
+            img = rospy.wait_for_message("/camera/rgb/image_raw", Image, 10)
+            print(type(img))
+        except Exception as e:
+            print(e)
 
 
