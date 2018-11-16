88f636659c9c745f4ad1ec5e56d30cd21b4b1468
Anina Mu <amu3@gatech.edu>
2018-11-16 16:12:09 -0500

    Replace blue layer with depth data

--- scripts/kinect_subscriber.py
+++ scripts/kinect_subscriber.py
@@ -2,19 +2,27 @@
 import rospy
 from sensor_msgs.msg import Image
 
+import numpy as np
 from PIL import Image as PILImage
 from cv_bridge import CvBridge, CvBridgeError
+
+IMAGE_TOPIC = "/camera/rgb/image_color"
+DEPTH_TOPIC = "/camera/depth/image_raw"
 
 
 def get_image(show=False):
     #print("CALLING GET_KINECT_IMAGE")
     rospy.init_node("kinect_subscriber")
-    image = rospy.wait_for_message("/camera/rgb/image_color", Image)
+    rgb = rospy.wait_for_message(IMAGE_TOPIC, Image)
+    depth = rospy.wait_for_message(DEPTH_TOPIC, Image)
 
     # Convert sensor_msgs.Image readings into readable format
     bridge = CvBridge()
-    image = bridge.imgmsg_to_cv2(image, image.encoding)
+    rgb = bridge.imgmsg_to_cv2(rgb, rgb.encoding)
+    depth = bridge.imgmsg_to_cv2(depth, depth.encoding)
 
+    image = rgb
+    image[:, :, 2] = depth
     if (show):
         im = PILImage.fromarray(image, 'RGB')
         im.show()
